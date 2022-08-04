637c286894a60c1c43c2050778af2e31c08b2746
Nadav <33123586+zaguri8@users.noreply.github.com>
2022-08-05 01:20:29 +0300

    csv-support-new-design

--- uploadgit.js
+++ uploadgit.js
@@ -1,8 +1,11 @@
 const { exec } = require('child_process');
 const { exit } = require('process');
-const commit_note = process.argv[2];
-const proc = exec(`npm run build && git add . && git commit -m '${commit_note}' && git push --force && firebase deploy`)
-proc.on('spawn',()=>{setTimeout(()=>{exit(0)},30 *1000)})
-proc.on('error', (error) => {
-    console.log(error)
-})
+const spawn = () => { setTimeout(() => { exit(0) }, 30 * 1000) }
+
+(function uploadToGit(commit_note) {
+    const upload_command = `npm run build && git add . && git commit -m '${commit_note}' && git push --force && firebase deploy`
+    exec(upload_command)
+        .on('spawn', spawn)
+        .on('message', console.log)
+        .on('error', console.log)
+})(process.argv[2] /* command execution argument - commit message */)
